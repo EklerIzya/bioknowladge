d7a40fd4fc07ce1ba3459ef8610a662c7109df2a
EklerIzya <lusmik020502@mail.ru>
2017-06-09 14:58:01 +0300

    List and knowladge
    kek

--- app/src/main/java/com/izya/ekler/bioknowladge/MainActivity.java
+++ app/src/main/java/com/izya/ekler/bioknowladge/MainActivity.java
@@ -1,13 +1,20 @@
 package com.izya.ekler.bioknowladge;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private String[] data = new  String[]{"Мозг","Кости","Мышцы"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +23,21 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        listView=(ListView)findViewById(R.id.list);
+        listView.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data));
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent intent = new Intent(MainActivity.this,KnowladgeActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putLong("id",id);
+                bundle.putString("title",data[(int)id]);
+                intent.putExtras(bundle);
+                startActivity(intent);
             }
         });
     }
+
 
 }
